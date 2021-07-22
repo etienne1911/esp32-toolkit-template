ced91de68734546cda4fa3c7938ee3d1ccfa7912
Etienne M <em85@pm.me>
2021-07-22 20:36:34 +0200

    whole reorg

--- README.md
+++ README.md
@@ -1,10 +1,69 @@
 # ESP32 Toolkit Template
 
-Codebase for esp32 projects containing common features.
+Aiming to be used as a base for new projects or sandbox...
 
 ## Embedded Features
-- Wifi Network: AP + STA
-- Internal Filesystem: SPIFFS
-- Webservices: Webserver, Websockets 
+- wifi network: AP + STA
+- internal filesystem: LittleFS
+- web services: Webserver, Websockets 
 - OTA updates
-- Power management: sleep/wake cycle
+- power management: sleep/wake cycle
+
+## Prereq
+
+VSCode + PlatformIO extension are required.
+
+In platform.ini `board` and `monitor_speed`  can be customized.
+
+ ## Configuration
+
+### Network settings
+
+Both AP (access point) and STA (wifi client) modes are supported and can work in the same time.
+
+Before building and uploading, configure wifi settings (SSID + password) for each modes.
+
+Once the board boots, the registered IP address will be displayed in the serial console.
+
+### OTA updates
+
+This feature permits to update device over-the-air without resorting to serial transfer.
+
+Note that you must first use classic method using serial communications before you can flash using OTA.
+
+Also make sure wifi settings are correctly setup in previous step to allow device to connect to the WLAN network.
+
+After building a new firmware, a `firmware.bin` file will be output in `.pio/build/<board-model>/firmware.bin` folder of the project.
+
+While device is booted and connected to your WLAN network, browse the following url `<deviceIP>/update` which should display an updater page.
+Select the firmware file that was output before to upload it to your board.
+Now the device should be updated with the latest version of your code
+
+Using the same method, you can also update device's internal storage(see Filesystem step).
+Simply choose `filesystem` option instead of `firmware` on the updater page, and select the filesystem image which will be available in `.pio/build/<board-model>/SPIFFS.bin` after building in the next step.
+
+### Filesystem
+
+The board is able to handle a small filesystem (LittleFS) to store files.
+This will be particularly handy when serving web pages through webserver (described at next step).
+To use it you'll need to put the files in the `data` folder of the project and build a filesystem image, 
+by running `<boardname> -> Platform -> Build Filesystem Image` task.
+This will output a `SPIFFS.bin` image in the `.pio/build/<board-model>/`, that you can then upload the the board either through serial or OTA updater.
+
+### Web Server
+
+To host static web pages, include the files you want to serve in the filesystem image from previous step.
+Upload the filesystem image to your device, and access each page by browsing file's url: `<deviceIp>/filename` 
+
+### Web Sockets
+
+Web socket protocol is also available on the board. 
+It is much lighter and faster than common http requests and particularly useful in remote control application where real-time connection are needed.
+
+### Power Management
+
+Coming soon.
+
+## Credits
+
+This template was mainly inspired from this [tutorial](https://randomnerdtutorials.com/esp32-ota-over-the-air-arduino/).
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,171 @@
+/**
+ * ESP32 Toolkit Template
+ * 
+ * Embedded Features:
+ * - Wifi Network: AP + STA
+ * - Embedded Filesystem: SPIFFS
+ * - Webservices: Webserver, Websockets 
+ * - OTA updates
+ * - Power management: sleep/wake cycle
+**/
+
+#include <iostream>
+
 #include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+// #include <Arduino_JSON.h>
+#include <AsyncElegantOTA.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char *hotspotSSID = "******";
+const char *hotspotPWD = "******";
+
+const char *wifiSSID = "******";
+const char *wifiPWD = "******";
+
+// using namespace std;
+
+AsyncWebServer server(80);
+
+AsyncWebSocket ws("/ws");
+
+
+/** 
+ * FileSystem 
+ **/
+void initSPIFFS() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
 }
 
+/** 
+ * Network 
+ * - Access Point Mode
+ * - Client mode
+**/
+void WifiAP()
+{
+  Serial.print("Setting AP (Access Point)...");
+  WiFi.softAP(hotspotSSID, hotspotPWD);
+  std::cout << "Setup with IP " << WiFi.softAPIP() << std::endl;
+}
+
+void WifiSTA()
+{
+  WiFi.begin(wifiSSID, wifiPWD);
+  std::cout << "Connecting to " << wifiSSID << " network..." << std::endl;
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  std::cout << std::endl
+       << "Connected with IP address: " << WiFi.localIP() << std::endl;
+}
+
+/** 
+ * WebServer
+ **/
+
+void initServerRoutes() {
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html",false);
+  });
+
+  server.serveStatic("/", SPIFFS, "/");
+}
+
+/** 
+ * WebSockets
+ **/
+
+String getOutputStates(){
+//   JSONVar myArray;
+//   for (int i =0; i<NUM_OUTPUTS; i++){
+//     myArray["gpios"][i]["output"] = String(outputGPIOs[i]);
+//     myArray["gpios"][i]["state"] = String(digitalRead(outputGPIOs[i]));
+//   }
+//   String jsonString = JSON.stringify(myArray);
+  return "";//jsonString;
+}
+
+void notifyClients(String state) {
+  ws.textAll(state);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "states") == 0) {
+      notifyClients(getOutputStates());
+    }
+    else{
+      int gpio = atoi((char*)data);
+      digitalWrite(gpio, !digitalRead(gpio));
+      notifyClients(getOutputStates());
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
+
+/**
+ * SETUP
+ */
+void setup(){
+  Serial.begin(115200);
+
+  Serial.println("** Filesystem **");
+  initSPIFFS();
+
+  Serial.println("** Network **");
+  // WifiAP();  // Access Point
+  WifiSTA(); // Wifi Client
+
+  Serial.println("** Webservices **");
+  // initWebSocket();
+  initServerRoutes();
+
+  Serial.println("** OTA Updater **");
+  AsyncElegantOTA.begin(&server);
+  
+  // Start server
+  server.begin();
+}
+/**
+ * MAIN LOOP
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+  AsyncElegantOTA.loop();
+  // ws.cleanupClients();
 }
+
